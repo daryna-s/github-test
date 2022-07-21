462b3e4a912f077e32738abeca49ae55a4574e86
daryna-s <lypenn@gmail.com>
2022-07-21 12:23:08 +0300

    add js.js

---
+++ js.js
@@ -0,0 +1,8 @@
+const apartment = {
+    imgUrl: "https://via.placeholder.com/640x480",
+    descr: "Spacious apartment in the city center",
+    rating: 4,
+    price: 2153,
+    tags: ["premium", "promoted", "top"]
+};
+console.log(apartment);
